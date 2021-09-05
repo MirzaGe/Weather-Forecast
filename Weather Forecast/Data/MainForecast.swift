1919b90b906ef3101baa90cf62b9131ea6321950
Sherry1481 <shareyscorpi1@gmail.com>
2021-09-05 12:43:59 +0500

    commit

--- Weather Forecast/Data/MainForecast.swift
+++ Weather Forecast/Data/MainForecast.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+import SwiftUI
+
+var ForecastWeatherResponse: Decodable {
+    var city: City? //we havent created so ? /* Bcuz it has its own properties & fields*/
+    var cod: String?
+    var message: Double?
+    var cnt: Int?
+    var list: [MainForecastParams]
+}
+
+struct MainForecastParams: Decodable {
+    var dt: Int
+    var temp: Temp?// temp is feild & also hav some objs which contains other fields
+    
+    
+    
+}
